dde7424910022d46404768f638b084d6d994e488
imadhubala27 <imadhubala27@gmail.com>
2025-05-07 11:47:42 +0530

    is gst

--- rp_app/customization/salesinvoice.py
+++ rp_app/customization/salesinvoice.py
@@ -2,9 +2,8 @@ import frappe
 from frappe import _
 
 def validate_gst_items(doc, method):
-    has_exempted = any(i.custom_is_gst_exempt for i in doc.items)
-    has_non_exempted = any(not i.custom_is_gst_exempt for i in doc.items)
+    has_exempted = any(item.custom_is_gst_exempt for item in doc.items)
+    has_non_exempted = any(not item.custom_is_gst_exempt for item in doc.items)
 
     if has_exempted and has_non_exempted:
         frappe.throw(_("GST Exempted and Non-Exempted items cannot be billed together."))
-
--- rp_app/public/js/salesinvoice.js
+++ rp_app/public/js/salesinvoice.js
@@ -1,12 +1,25 @@
+frappe.ui.form.on("Sales Invoice Item", {
+    item_code: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (row.item_code) {
+            frappe.db.get_value("Item", row.item_code, "custom_is_gst_exempted", (r) => {
+                if (r && r.custom_is_gst_exempted !== undefined) {
+                    frappe.model.set_value(cdt, cdn, "custom_is_gst_exempt", r.custom_is_gst_exempted);
+                }
+            });
+        }
+    }
+});
+
 frappe.ui.form.on("Sales Invoice", {
     validate: function(frm) {
         let has_gst_exempted = false;
         let has_non_exempted = false;
 
         (frm.doc.items || []).forEach(item => {
-            console.log("Item:", item.item_code, "| GST Exempted:", item.custom_is_gst_exempt);
+            const is_exempt = Boolean(item.custom_is_gst_exempt);
 
-            if (item.custom_is_gst_exempt) {
+            if (is_exempt) {
                 has_gst_exempted = true;
             } else {
                 has_non_exempted = true;
